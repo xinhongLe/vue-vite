7599691fb4b11b0aa3bf6b6a7482eb8b1c3a3de7
李新红 <lixinhong@upplus.net>
2024-03-07 17:22:37 +0800

    slot

---
+++ src/views/slot/components/slot-demo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "slot-demo"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/slot/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <slot-demo></slot-demo>
+    </div>
+</template>
+
+<script>
+import SlotDemo from "./components/slot-demo";
+export default {
+    name: "index",
+    components: {SlotDemo}
+}
+</script>
+
+<style scoped>
+
+</style>
